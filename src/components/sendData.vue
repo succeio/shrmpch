da181ad745633fa6c38b1e366abddfee19634475
Haisenx <agnusag@mail.ru>
2024-10-13 15:24:44 +0300

    obscuf 0.2

--- src/components/sendData.vue
+++ src/components/sendData.vue
@@ -81,7 +81,7 @@ const sendPost = async () => {
       const newPost = {
         name: postName.value ? (postName.value.length > 25 ? postName.value.slice(0, 25) : postName.value) : 'Аноним',
         password: postPassword.value ? hashedString.value : '',
-        theme: postTheme.value.length < 25 ? postTheme.value : postTheme.value.slice(0, 25),
+        theme: postTheme.value.length < 45 ? postTheme.value : postTheme.value.slice(0, 25),
         text: postText.value,
         url: postUrl.value.length < 100 ? (imgSize.value !== 0 ? (imgSize.value < 4000000 ? postUrl.value : '') : postUrl.value) : '',
         time: new Date().toLocaleTimeString('ru-RU', {
@@ -173,7 +173,7 @@ const sendPost = async () => {
       const newPost = {
         name: postName.value ? (postName.value.length > 25 ? postName.value.slice(0, 25) : postName.value) : 'Аноним',
         password: postPassword.value ? hashedString.value : '',
-        theme: postTheme.value.length < 25 ? postTheme.value : postTheme.value.slice(0, 25),
+        theme: postTheme.value.length < 45 ? postTheme.value : postTheme.value.slice(0, 25),
         text: postText.value,
         url: postUrl.value.length < 100 ? postUrl.value : '',
         time: new Date().toLocaleTimeString('ru-RU', {
